b0e3034532f6cd5156aff595d0b107578ceea0e2
Blazej <a.blazej8@gmail.com>
2021-11-23 13:48:12 +0100

    poprawka zwracania listy - bez pierwszego elementu

--- utils/readFiles.py
+++ utils/readFiles.py
@@ -7,4 +7,4 @@ def get_data():
         for i in file:
             row = i.strip().split(',')
             object_list.append(m(row[0], row[1], row[2]).__dict__)
-    return object_list
+    return object_list[1:]
