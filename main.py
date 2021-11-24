bb8ddbd8d6bdb6852bd815eb6cd61c8bf4d1dde4
Blazej <a.blazej8@gmail.com>
2021-11-24 11:22:01 +0100

    czytanie kilku plikow

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 from flask import Flask
 from flask_restful import Resource, Api
 
-from utils.readFiles import get_data as file
+from Models.Movie import Movie
+from Models.Rating import Rating
+from utils.readFiles import *
+from Models.Links import Link
+from Models.Tag import Tag
 
 app = Flask(__name__)
 api = Api(app)
 
 
-class HelloWorld(Resource):
+class GetMovies(Resource):
 
     def get(self):
-        return file()
+        objects_list = []
+        for i in get_data(read_movies()):
+            objects_list.append(Movie(i[0], i[1], i[2]).__dict__)
+        return objects_list
 
 
-api.add_resource(HelloWorld, '/')
+class GetLinks(Resource):
+
+    def get(self):
+        objects_list = []
+        for i in get_data(read_links()):
+            objects_list.append(Link(i[0], i[1], i[2]).__dict__)
+        return objects_list
+
+
+class GetRatings(Resource):
+
+    def get(self):
+        objects_list = []
+        for i in get_data(read_ratings()):
+            objects_list.append(Rating(i[0], i[1], i[2], i[3]).__dict__)
+        return objects_list
+
+
+class GetTags(Resource):
+
+    def get(self):
+        objects_list = []
+        for i in get_data(read_tags()):
+            objects_list.append(Tag(i[0], i[1], i[2], i[3]).__dict__)
+        return objects_list
+
+
+api.add_resource(GetMovies, '/movies')
+api.add_resource(GetLinks, '/links')
+api.add_resource(GetRatings, '/ratings')
+api.add_resource(GetTags, '/tags')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- utils/readFiles.py
+++ utils/readFiles.py
@@ -1,10 +1,26 @@
-from Movie import Movie as m
+from Models.Links import Link
 
 
-def get_data():
-    object_list = []
-    with open('data/movies.csv', encoding="utf8") as file:
+def get_data(open_file):
+    rows_list = []
+    with open_file as file:
         for i in file:
             row = i.strip().split(',')
-            object_list.append(m(row[0], row[1], row[2]).__dict__)
-    return object_list[1:]
+            rows_list.append(row)
+    return rows_list[1:]
+
+
+def read_movies():
+    return open('data/movies.csv', encoding="utf8")
+
+
+def read_links():
+    return open('data/links.csv', encoding="utf8")
+
+
+def read_ratings():
+    return open('data/ratings.csv', encoding="utf8")
+
+
+def read_tags():
+    return open('data/tags.csv', encoding="utf8")
