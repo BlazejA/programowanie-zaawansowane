318536e0e9932d5c2348815f120450806aa2184b
Blazej <a.blazej8@gmail.com>
2022-01-26 13:29:43 +0100

    Added reading files method

---
+++ Detector.py
@@ -0,0 +1,36 @@
+import cv2
+import imutils
+
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+
+def detect_people(image_path: str):
+    img = cv2.imread(image_path)
+    img = imutils.resize(img,
+                         width=min(400, img.shape[1]))
+
+    (frame, _) = hog.detectMultiScale(img,
+                                      winStride=(4, 4),
+                                      padding=(8, 8),
+                                      scale=1.05)
+
+    number_of_people = 0
+    for x, y, w, h in frame:
+        cv2.rectangle(img,
+                      (x, y),
+                      (x + w, y + h),
+                      (0, 255, 0),
+                      2)
+        number_of_people += 1
+    cv2.putText(img,
+                f'{number_of_people} person detected',
+                (20, 30),
+                cv2.FONT_HERSHEY_DUPLEX,
+                1,
+                (255, 0, 0),
+                2)
+
+    cv2.imshow("Person detector", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-import cv2
-import imutils
+import glob
 
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+from Detector import detect_people
 
 
-def detect_people(image_path: str):
-    img = cv2.imread(image_path)
-    img = imutils.resize(img,
-                         width=min(400, img.shape[1]))
-
-    (frame, _) = hog.detectMultiScale(img,
-                                      winStride=(4, 4),
-                                      padding=(8, 8),
-                                      scale=1.05)
-
-    number_of_people = 0
-    for x, y, w, h in frame:
-        cv2.rectangle(img,
-                      (x, y),
-                      (x + w, y + h),
-                      (0, 255, 0),
-                      2)
-        number_of_people += 1
-    cv2.putText(img,
-                f'{number_of_people} person detected',
-                (20, 30),
-                cv2.FONT_HERSHEY_DUPLEX,
-                1,
-                (255, 0, 0),
-                2)
-
-    cv2.imshow("Person detector", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+def get_photo_paths():
+    photos_path = []
+    for img in glob.glob("photos/*.jpg"):
+        photos_path.append(img)
+    return photos_path
 
 
 if __name__ == '__main__':
-    detect_people("photos/photo1.jpg")
-    detect_people("photos/photo2.jpg")
-    detect_people("photos/photo3.jpg")
-    detect_people("photos/photo4.jpg")
+    for path in get_photo_paths():
+        detect_people(path)
